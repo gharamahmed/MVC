33699c23a8094fb9ff63f83558fab64f39c9a23e
Gharam12721 <gharamahmed127@outlook.com>
2023-01-29 23:11:36 +0200

    Day5

---
+++ MVC2/Controllers/DepartmentHelperController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MVC2.Models;
+
+namespace MVC2.Controllers
+{
+    public class DepartmentHelperController : Controller
+    {
+        private CompanyContext context;
+
+        public DepartmentHelperController()
+        {
+            context= new CompanyContext();
+        }
+        public IActionResult Index()
+        {
+            List<department>departments=context.Departments.ToList();
+            return View(departments);
+        }
+        public IActionResult Add()
+        {
+            List<employee> employees = context.Employees.ToList();
+            ViewBag.Employees = new SelectList(employees, "SSN", "Fname");
+            return View();
+        }
+        public IActionResult AddDepartment(department department)
+        {
+            context.Departments.Add(department);
+            context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Edit(int id)
+        {
+            department OldDepartment = context.Departments.SingleOrDefault(i => i.Number == id);
+            List<employee> Employees = context.Employees.ToList();
+            //ViewBag.courses = new SelectList(courses, "Id", "Name");
+            ViewBag.Employees = new SelectList(Employees, "SSN", "Fname");
+            return View("Edit", OldDepartment);
+        }
+
+        public IActionResult EditDepartment(department department)
+        {
+            var OldDepartment = context.Departments.SingleOrDefault(e => e.Number == department.Number);
+            OldDepartment.Name= department.Name;
+            OldDepartment.startdate= department.startdate;
+            OldDepartment.employeeSSN= department.employeeSSN;
+            context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ MVC2/Controllers/ProjectHelperController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MVC2.Models;
+namespace MVC2.Controllers
+{
+    public class ProjectHelperController : Controller
+    {
+        private CompanyContext context;
+        public ProjectHelperController()
+        {
+            context = new CompanyContext();
+        }
+       
+        public IActionResult Index()
+        {
+            List<project>projects=context.Projects.ToList();
+            return View(projects);
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            List<department>departments=context.Departments.ToList();
+            ViewBag.departments = new SelectList(departments, "Number", "Name");
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddProject(project project)
+        {
+            context.Projects.Add(project);
+            context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            project Oldproject = context.Projects.SingleOrDefault(i => i.Number == id);
+            List <department> Departments = context.Departments.ToList();
+            //ViewBag.courses = new SelectList(courses, "Id", "Name");
+            ViewBag.departments = new SelectList(Departments, "Number", "Name");
+            return View("Edit",Oldproject);
+        }
+        [HttpPost]
+        public IActionResult EditProject(project project)
+        {
+            var Oldproject = context.Projects.SingleOrDefault(e => e.Number == project.Number);
+            Oldproject.Name = project.Name;
+            Oldproject.Location = project.Location;
+            Oldproject.DepartmentId = project.DepartmentId;
+            context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
